4e1eff59d5114dfd2479545970b0b5e64cbf881e
Hai Le <thanh-h.le@broadcom.com>
2019-11-19 13:40:36 +0700

    a

---
+++ long-running-template/main.tf
@@ -0,0 +1,26 @@
+#**********************************************************************
+#                        Docker provider
+#**********************************************************************
+provider "docker" {
+  host = "${var.docker_host}" 
+}
+
+resource "random_string" "long_running" {    
+    
+	length  = 10
+	special = false
+	lower   = false
+	
+    provisioner "local-exec" {
+      command = "ping 127.0.0.1 -n ${var.ping_time}"
+    }
+}
+
+variable "ping_time" {
+  default = "100000"
+}
+
+variable "docker_host" {
+  default = "http://vvnappsrv04:4243"
+}
+
